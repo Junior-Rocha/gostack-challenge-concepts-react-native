7a54e7206c9797ce03e1a0b9c48b3cb61b7b5de4
Junior Rocha <rochajunior93@hotmail.com>
2020-05-11 20:30:15 -0300

    challenge resolved

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import Repository from './src/App'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+   <Repository/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
